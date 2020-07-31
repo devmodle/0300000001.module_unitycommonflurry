85990f90cf694a425af322111619a1f2b245fb08
sd.lee <sd.lee@ninetap.com>
2020-07-31 17:42:05 +0900

    [이상동] 플러리 관리자 스크립트 분할

---
+++ Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if FLURRY_ENABLE && FLURRY_ANALYTICS_ENABLE
+using FlurrySDK;
+
+//! 플러리 관리자
+public partial class CFlurryManager : CSingleton<CFlurryManager> {
+	#region 함수
+	//! 유저 식별자를 변경한다
+	public void SetUserID(string a_oID) {
+		CAccess.Assert(a_oID.ExIsValid());
+		CFunc.ShowLog("CFlurryManager.SetUserID: {0}", KCDefine.B_LOG_COLOR_PLUGIN, a_oID);
+
+		if(this.IsInit) {
+			Flurry.SetUserId(a_oID);
+		}
+	}
+
+	//! 로그를 전송한다
+	public void SendLog(string a_oName) {
+		this.SendLog(a_oName, null);
+	}
+
+	//! 로그를 전송한다
+	public void SendLog(string a_oName, string a_oParam, List<string> a_oDataList) {
+		CAccess.Assert(a_oParam.ExIsValid());
+
+		this.SendLog(a_oName, new Dictionary<string, string>() {
+			[a_oParam] = a_oDataList.ExToString(KCDefine.U_TOKEN_FLURRY_ANALYTICS_LOG_DATA)
+		});
+	}
+
+	//! 로그를 전송한다
+	public void SendLog(string a_oName, Dictionary<string, string> a_oDataList) {
+		CAccess.Assert(a_oName.ExIsValid());
+		CFunc.ShowLog("CFlurryManager.SendLog: {0}, {1}", KCDefine.B_LOG_COLOR_PLUGIN, a_oName, a_oDataList);
+
+#if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+		if(this.IsInit) {
+			var oDataList = a_oDataList ?? new Dictionary<string, string>();
+
+#if MSG_PACK_ENABLE
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_DEVICE_ID, CAppInfoStorage.Instance.AppInfo.DeviceID);
+
+#if AUTO_LOG_PARAM_ENABLE
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, CAppInfoStorage.Instance.PlatformName);
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, CUserInfoStorage.Instance.UserInfo.UserType.ToString());
+			
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_LOG_TIME, System.DateTime.UtcNow.ExToLongString());
+			oDataList.ExAddValue(KCDefine.U_LOG_KEY_INSTALL_TIME, CAppInfoStorage.Instance.AppInfo.UTCInstallTime.ExToLongString());
+#endif			// #if AUTO_LOG_PARAM_ENABLE
+#endif			// #if MSG_PACK_ENABLE
+
+			Flurry.LogEvent(a_oName, oDataList);
+		}
+#endif			// #if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
+	}
+	#endregion			// 함수
+}
+#endif			// #if FLURRY_ENABLE && FLURRY_ANALYTICS_ENABLE
--- Scripts/Runtime/Manager/CFlurryManager.cs
+++ Scripts/Runtime/Manager/CFlurryManager.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using FlurrySDK;
 
 //! 플러리 관리자
-public class CFlurryManager : CSingleton<CFlurryManager> {
+public partial class CFlurryManager : CSingleton<CFlurryManager> {
 	#region 프로퍼티
 	public bool IsInit { get; private set; } = false;
 	#endregion			// 프로퍼티
@@ -51,59 +51,5 @@ public class CFlurryManager : CSingleton<CFlurryManager> {
 		a_oCallback?.Invoke(this, this.IsInit);
 	}
 	#endregion			// 함수
-
-	#region 조건부 함수
-#if FLURRY_ANALYTICS_ENABLE
-	//! 유저 식별자를 변경한다
-	public void SetUserID(string a_oID) {
-		CAccess.Assert(a_oID.ExIsValid());
-		CFunc.ShowLog("CFlurryManager.SetUserID: {0}", KCDefine.B_LOG_COLOR_PLUGIN, a_oID);
-
-		if(this.IsInit) {
-			Flurry.SetUserId(a_oID);
-		}
-	}
-
-	//! 로그를 전송한다
-	public void SendLog(string a_oName) {
-		this.SendLog(a_oName, null);
-	}
-
-	//! 로그를 전송한다
-	public void SendLog(string a_oName, string a_oParam, List<string> a_oDataList) {
-		CAccess.Assert(a_oParam.ExIsValid());
-
-		this.SendLog(a_oName, new Dictionary<string, string>() {
-			[a_oParam] = a_oDataList.ExToString(KCDefine.U_TOKEN_FLURRY_ANALYTICS_LOG_DATA)
-		});
-	}
-
-	//! 로그를 전송한다
-	public void SendLog(string a_oName, Dictionary<string, string> a_oDataList) {
-		CAccess.Assert(a_oName.ExIsValid());
-		CFunc.ShowLog("CFlurryManager.SendLog: {0}, {1}", KCDefine.B_LOG_COLOR_PLUGIN, a_oName, a_oDataList);
-
-#if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
-		if(this.IsInit) {
-			var oDataList = a_oDataList ?? new Dictionary<string, string>();
-
-#if MSG_PACK_ENABLE
-			oDataList.ExAddValue(KCDefine.U_LOG_KEY_DEVICE_ID, CAppInfoStorage.Instance.AppInfo.DeviceID);
-
-#if AUTO_LOG_PARAM_ENABLE
-			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, CAppInfoStorage.Instance.PlatformName);
-			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, CUserInfoStorage.Instance.UserInfo.UserType.ToString());
-			
-			oDataList.ExAddValue(KCDefine.U_LOG_KEY_LOG_TIME, System.DateTime.UtcNow.ExToLongString());
-			oDataList.ExAddValue(KCDefine.U_LOG_KEY_INSTALL_TIME, CAppInfoStorage.Instance.AppInfo.UTCInstallTime.ExToLongString());
-#endif			// #if AUTO_LOG_PARAM_ENABLE
-#endif			// #if MSG_PACK_ENABLE
-
-			Flurry.LogEvent(a_oName, oDataList);
-		}
-#endif			// #if ANALYTICS_TEST_ENABLE || (ADHOC_BUILD || STORE_BUILD)
-	}
-#endif			// #if FLURRY_ANALYTICS_ENABLE
-	#endregion			// 조건부 함수
 }
 #endif			// #if FLURRY_ENABLE
