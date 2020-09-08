d20813a215510969063cacebb019f99a686cd8d8
dante <are2341@nate.com>
2020-09-09 03:11:35 +0900

    [이상동] 패키지 갱신

--- Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
+++ Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
@@ -46,13 +46,13 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 #if MSG_PACK_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_DEVICE_ID, CCommonAppInfoStorage.Instance.AppInfo.DeviceID);
 
-#if AUTO_LOG_PARAM_ENABLE
+#if AUTO_LOG_PARAMS_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, CCommonAppInfoStorage.Instance.PlatformName);
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, CCommonUserInfoStorage.Instance.UserInfo.UserType.ToString());
 			
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_LOG_TIME, System.DateTime.UtcNow.ExToLongString());
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_INSTALL_TIME, CCommonAppInfoStorage.Instance.AppInfo.UTCInstallTime.ExToLongString());
-#endif			// #if AUTO_LOG_PARAM_ENABLE
+#endif			// #if AUTO_LOG_PARAMS_ENABLE
 #endif			// #if MSG_PACK_ENABLE
 
 			Flurry.LogEvent(a_oName, oDataList);
