8088451b3dfdb61564b9697d3897fee25132ce84
sd.lee <sd.lee@ninetap.com>
2020-11-06 17:23:33 +0900

    [이상동] 로그 관련 로직 수정

--- Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
+++ Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
@@ -50,10 +50,10 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_DEVICE_ID, 
 				CCommonAppInfoStorage.Instance.AppInfo.DeviceID);
 
-#if AUTO_LOG_PARAMS_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_PLATFORM, 
 				CCommonAppInfoStorage.Instance.Platform);
 
+#if AUTO_LOG_PARAMS_ENABLE
 			oDataList.ExAddValue(KCDefine.U_LOG_KEY_USER_TYPE, 
 				CCommonUserInfoStorage.Instance.UserInfo.UserType.ToString());
 			
