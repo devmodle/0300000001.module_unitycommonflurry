f7522553c27e6d4767976eb1a621dd829728283a
sd.lee <sd.lee@ninetap.com>
2021-10-07 15:02:36 +0900

    [이상동] 버전 관련 로직 수정

--- Scripts/Runtime/Manager/CFlurryManager.cs
+++ Scripts/Runtime/Manager/CFlurryManager.cs
@@ -45,7 +45,7 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 			oBuilder.WithMessaging(false);
 			oBuilder.WithLogLevel(Flurry.LogLevel.VERBOSE);
 			oBuilder.WithContinueSessionMillis(KCDefine.U_TIMEOUT_FLURRY_M_NETWORK_CONNECTION);
-			oBuilder.WithAppVersion(CProjInfoTable.Inst.ProjInfo.m_stBuildVer.m_oVer);
+			oBuilder.WithAppVersion(CProjInfoTable.Inst.ProjInfo.m_stBuildVerInfo.m_oVer);
 			oBuilder.WithDataSaleOptOut(!CCommonAppInfoStorage.Inst.AppInfo.IsAgreeTracking);
 
 #if FLURRY_ANALYTICS_ENABLE && (ANALYTICS_TEST_ENABLE || ADHOC_BUILD || STORE_BUILD)
