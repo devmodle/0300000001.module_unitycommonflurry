b435fed8c84da594b054aac38c821360efa194e0
sd.lee <sd.lee@ninetap.com>
2022-04-02 14:55:11 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CFlurryManager.cs
+++ Scripts/Runtime/Manager/CFlurryManager.cs
@@ -35,7 +35,7 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 		CFunc.ShowLog($"CFlurryManager.Init: {a_stParams.m_oAPIKey}", KCDefine.B_LOG_COLOR_PLUGIN);
 		CAccess.Assert(a_stParams.m_oAPIKey.ExIsValid());
 
-#if UNITY_IOS || UNITY_ANDROID
+#if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
 		// 초기화 되었을 경우
 		if(this.IsInit) {
 			a_stParams.m_oCallbackDict?.GetValueOrDefault(ECallback.INIT)?.Invoke(this, true);
@@ -67,7 +67,7 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 		}
 #else
 		a_stParams.m_oCallbackDict?.GetValueOrDefault(ECallback.INIT)?.Invoke(this, false);
-#endif			// #if UNITY_IOS || UNITY_ANDROID
+#endif			// #if !UNITY_EDITOR && (UNITY_IOS || UNITY_ANDROID)
 	}
 	#endregion			// 함수
 
