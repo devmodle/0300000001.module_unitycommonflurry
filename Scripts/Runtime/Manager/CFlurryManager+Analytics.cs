2d1f1ece5ce8544f9f33ba66cee8ac83225ebb2f
sd.lee <sd.lee@ninetap.com>
2022-05-09 17:57:55 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
+++ Scripts/Runtime/Manager/CFlurryManager+Analytics.cs
@@ -43,7 +43,7 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 	#region 조건부 함수
 #if PURCHASE_MODULE_ENABLE
 	/** 결제 로그를 전송한다 */
-	public void SendPurchaseLog(Product a_oProduct, int a_nNumProducts) {
+	public void SendPurchaseLog(Product a_oProduct, int a_nNumProducts, Dictionary<string, string> a_oDataDict) {
 		CFunc.ShowLog($"CFlurryManager.SendPurchaseLog: {a_oProduct}, {a_nNumProducts}", KCDefine.B_LOG_COLOR_PLUGIN);
 		CAccess.Assert(a_oProduct != null && a_nNumProducts > KCDefine.B_VAL_0_INT);
 
@@ -51,7 +51,7 @@ public partial class CFlurryManager : CSingleton<CFlurryManager> {
 		// 초기화 되었을 경우
 		if(this.IsInit) {
 			double dblPrice = decimal.ToDouble(a_oProduct.metadata.localizedPrice);
-			Flurry.LogPayment(a_oProduct.metadata.localizedTitle, a_oProduct.definition.id, a_nNumProducts, dblPrice, a_oProduct.metadata.isoCurrencyCode, a_oProduct.transactionID, null);
+			Flurry.LogPayment(a_oProduct.metadata.localizedTitle, a_oProduct.definition.id, a_nNumProducts, dblPrice, a_oProduct.metadata.isoCurrencyCode, a_oProduct.transactionID, a_oDataDict);
 		}
 #endif			// #if ((UNITY_IOS || UNITY_ANDROID) && FLURRY_ANALYTICS_ENABLE) && (ANALYTICS_TEST_ENABLE || STORE_DIST_BUILD)
 	}
